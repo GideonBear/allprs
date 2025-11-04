a2a90496cc86d181c21bf580c4cdadce6b16d43e
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-11-04 21:10:55 +0100

    feat: Add config file, and remove index-lines from diffs for better diff equality

--- src/allprs/__init__.py
+++ src/allprs/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import operator
+import re
 import subprocess
 import sys
 import webbrowser
@@ -13,24 +14,14 @@ from github import Auth, Github
 from readchar import readchar
 
 from allprs import config
+from allprs.config import pr_queries
 from allprs.utils import clear as clear, group_by, print_line
 
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Iterator, Sequence
+    from collections.abc import Iterator, Sequence
 
     from github.PullRequest import PullRequest
-
-
-pr_queries: list[tuple[str, Callable[[str], bool]]] = [
-    ("author:app/pre-commit-ci", lambda _b: True),
-    ("author:app/renovate", lambda _b: True),
-    ("head:pre-commit-autoupdate", lambda _b: True),
-    (
-        "author:@me",
-        lambda b: b.startswith("all-repos_autofixer_"),
-    ),
-]
 
 
 def main() -> None:
@@ -41,7 +32,8 @@ def main() -> None:
     )
     auth = Auth.Token(token)
     with Github(auth=auth) as g:
-        for pr_query, head_check in pr_queries:
+        for pr_query_data in pr_queries:
+            pr_query = pr_query_data["query"]
             clear()
             print(f"Querying {pr_query}...")
             all_prs: Iterator[PullRequest] = (
@@ -50,7 +42,13 @@ def main() -> None:
                     f"is:pr state:open {config.repo_query} {pr_query}"
                 )
             )
-            all_prs = filter(lambda pr: head_check(pr.head), all_prs)  # type: ignore[arg-type]  # mypy bug
+            if pr_query_data.get("head_branch_regex"):
+                all_prs = filter(
+                    lambda pr: re.match(
+                        pr_query_data.get("head_branch_regex"), pr.head.label
+                    ),
+                    all_prs,
+                )
 
             title_groups = group_by(operator.attrgetter("title"), all_prs)
             for title, title_prs in title_groups.items():
@@ -60,6 +58,11 @@ def main() -> None:
                 )
                 for diff, diff_prs in diff_groups.items():
                     process_diff_group(title, diff, diff_prs)
+                    # if ret == CLOSE_TITLEGROUP:
+                    #     for
+
+
+# CLOSE_TITLEGROUP = object()
 
 
 def process_diff_group(
@@ -88,8 +91,11 @@ def process_diff_group(
         print(diff)
     print()
 
+    ret = None
+
     while True:
-        print("(a)ccept/(o)pen/(q)uit ", end="")
+        # print("(a)ccept/(o)pen/(s)kip/(c)lose/(q)uit ", end="")
+        print("(a)ccept/(o)pen/(s)kip/(q)uit ", end="")
         sys.stdout.flush()
         answer = readchar()
         print()
@@ -103,12 +109,36 @@ def process_diff_group(
         elif answer == "o":  # noqa: RET508
             print("Opening random PR from diff group...")
             webbrowser.open(diff_prs[0].html_url)
+        elif answer == "s":
+            break
+        # elif answer == "c":
+        #     done = False
+        #     while True:
+        #         print("(t)itlegroup/(d)iffgroup/(c)ancel ", end="")
+        #         sys.stdout.flush()
+        #         answer = readchar()
+        #         print()
+        #         if answer == "t":
+        #             ret = CLOSE_TITLEGROUP
+        #             answer = "d"
+        #         if answer == "d":
+        #             for pr in diff_prs:
+        #                 print(f"Closing for {pr.base.repo.full_name}...")
+        #                 pr.edit(state="closed")
+        #         elif answer == "c":
+        #             break
+        #         else:
+        #             print("Invalid answer")
+        #
+        #     if done:
+        #         break
         elif answer in {"q", "\x03"}:
             sys.exit(3)
         else:
             print("Invalid answer")
 
     clear()
+    return ret
 
 
 def check_status(pr: PullRequest) -> None:
@@ -117,6 +147,10 @@ def check_status(pr: PullRequest) -> None:
         if state == "success":
             return
         elif state == "pending":  # noqa: RET505
+            # TODO(GideonBear): When we have pre-commit lite  # noqa: FIX002, TD003
+            #  running we can remove this, as we then always have status checks then
+            if len(list(list(pr.get_commits())[-1].get_statuses())) == 0:
+                return
             print(f"Status check: {state}. Sleeping 5s...")
             sleep(5)
         else:
@@ -135,4 +169,8 @@ def get_diff(pr: PullRequest, token: str) -> str:
         },
     )
     resp.raise_for_status()
-    return resp.text
+    return fix_diff(resp.text)
+
+
+def fix_diff(diff: str) -> str:
+    return "\n".join(line for line in diff.split("\n") if not line.startswith("index"))
