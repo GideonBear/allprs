aba3df88af1811a601be0795ab0dcabaf912ca4e
Gideon <dig-sultry-sedate@duck.com>
2026-02-14 16:40:30 +0100

    feat: error on erroneous config keys

--- src/allprs/config.py
+++ src/allprs/config.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+import sys
 from pathlib import Path
 
 
@@ -11,12 +12,12 @@ if path.exists():
 else:
     data = {}
 
-repo_query = data.get("repo_query", "user:@me archived:false")
-repo_query_extend = data.get("repo_query_extend")
+repo_query = data.pop("repo_query", "user:@me archived:false")
+repo_query_extend = data.pop("repo_query_extend", None)
 if repo_query_extend:
     repo_query += f" {repo_query_extend}"
 
-pr_queries = data.get(
+pr_queries = data.pop(
     "pr_queries",
     [
         {"query": "author:app/pre-commit-ci"},
@@ -25,6 +26,8 @@ pr_queries = data.get(
         {"query": "author:@me", "head_branch_regex": "^all-repos_autofix_.*$"},
     ],
 )
-pr_queries_extend = data.get("pr_queries_extend")
-if pr_queries_extend:
-    pr_queries.extend(pr_queries_extend)
+pr_queries.extend(data.pop("pr_queries_extend", ()))
+
+if data:
+    print(f"ERROR: found extra configuration key(s): {', '.join(data)}")
+    sys.exit(2)
